cb3bf2555f5b4f9796b0af4c97e3068b22a47583
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 03:51:04 +0300

    Replace `NavLink` with `NavigationLink` component.

--- src/components/layout/SignedOutLinks.jsx
+++ src/components/layout/SignedOutLinks.jsx
@@ -1,15 +1,11 @@
-import { NavLink } from "react-router-dom";
+import NavigationLink from "../shared/NavigationLink";
 
 const SignedOutLinks = () => {
   return (
     <>
       <div className="flex items-center text-lg font-semibold">
-        <NavLink className="py-8 px-2 hover:bg-slate-700" to="/">
-          Signup
-        </NavLink>
-        <NavLink className="py-8 px-2 hover:bg-slate-700" to="/">
-          Login
-        </NavLink>
+        <NavigationLink to="/" text="Sign Up" />
+        <NavigationLink to="/" text="Login" />
       </div>
     </>
   );
