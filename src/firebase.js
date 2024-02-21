f522f2cfce0d168b13aace4ce157ac34d4412a09
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 02:31:48 +0300

    Configure firebase

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA6PDl6LkiDAB9l1_Nf4GQEvo2Tr7gs-TQ",
+  authDomain: "fir-post-app-ee9f5.firebaseapp.com",
+  projectId: "fir-post-app-ee9f5",
+  storageBucket: "fir-post-app-ee9f5.appspot.com",
+  messagingSenderId: "507401262466",
+  appId: "1:507401262466:web:6108a2c50a0b9d6961ab9b",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
