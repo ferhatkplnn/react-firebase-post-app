e3e2c9da8f91486f4d17b7eed1a3ff7062721cee
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 03:54:31 +0300

    Adds a shadow to the navbar.

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -3,7 +3,7 @@ import SignedInLinks from "./SignedInLinks";
 import SignedOutLinks from "./SignedOutLinks";
 
 const styles = {
-  nav: `bg-slate-800 text-slate-200`,
+  nav: `bg-slate-800 text-slate-200 shadow-xl`,
   container: `container mx-auto  flex justify-between items-center `,
   logo: `text-6xl font-semibold`,
 };
