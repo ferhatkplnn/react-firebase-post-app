13a76eeb540b53a1abeb290de0325d85aa5197a5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 03:43:57 +0300

    Create Navbar component and add sign links

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,26 @@
+import { Link } from "react-router-dom";
+import SignedInLinks from "./SignedInLinks";
+import SignedOutLinks from "./SignedOutLinks";
+
+const styles = {
+  nav: `bg-slate-800 text-slate-200`,
+  container: `container mx-auto  flex justify-between items-center `,
+  logo: `text-6xl font-semibold`,
+};
+
+const Navbar = () => {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <Link className={styles.logo} to="/">
+          Splash Posts
+        </Link>
+
+        <SignedOutLinks />
+        <SignedInLinks />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
