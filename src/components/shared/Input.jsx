b16dde99a5f523c57ef84de32608928106c045a9
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 05:46:55 +0300

    Create Input component

---
+++ src/components/shared/Input.jsx
@@ -0,0 +1,16 @@
+const Input = ({ type, placeholder, value, handleChange, ...props }) => {
+  return (
+    <>
+      <input
+        value={value}
+        onChange={handleChange}
+        type={type}
+        placeholder={placeholder}
+        className="outline-none border-b-2 py-4 px-2 border-slate-200 focus:border-sky-300"
+        {...props}
+      />
+    </>
+  );
+};
+
+export default Input;
