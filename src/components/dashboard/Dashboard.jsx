709ace7d8a40727691d24a06ad47a3391615d83a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 21:35:44 +0300

    Remove two duplicate instances of the `ProjectList` component.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,8 +6,6 @@ const Dashboard = () => {
     <div className=" container mx-auto flex justify-between gap-40 p-8">
       <div className="flex flex-col space-y-8 basis-1/2">
         <ProjectList />
-        <ProjectList />
-        <ProjectList />
       </div>
       <div className="basis-1/2">
         <Notifications />
