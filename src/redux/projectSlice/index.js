918fc67e44e28fe7375714df0479d1cfccba5df1
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 21:35:00 +0300

    Set initialState for project slice

--- src/redux/projectSlice/index.js
+++ src/redux/projectSlice/index.js
@@ -1,8 +1,16 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+  projects: [
+    { id: "1", title: "help me find peach", content: "bla bla bla" },
+    { id: "2", title: "collect all the starts", content: "bla bla bla" },
+    { id: "3", title: "egg hunt with yoshi", content: "bla bla bla" },
+  ],
+};
+
 const projectSlice = createSlice({
   name: "project",
-  initialState: {},
+  initialState,
   reducers: {},
 });
 
