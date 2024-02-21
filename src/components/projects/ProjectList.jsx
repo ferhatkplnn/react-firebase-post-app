41ee3690aaff8695314f717a071fc2d8c1554e31
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-21 04:40:03 +0300

    Update project list component to use ProjectSummary component

--- src/components/projects/ProjectList.jsx
+++ src/components/projects/ProjectList.jsx
@@ -1,11 +1,9 @@
+import ProjectSummary from "./ProjectSummary";
+
 const ProjectList = () => {
   return (
     <>
-      <div className="border flex flex-col p-12 bg-white drop-shadow-lg rounded-md space-y-1">
-        <h2 className="text-4xl font-medium mb-4">Yoshi's Egg hunt</h2>
-        <p className="text-xl">Posted By Mario Plummer</p>
-        <p className="text-lg text-slate-400">Today at 3:26 PM</p>
-      </div>
+      <ProjectSummary />
     </>
   );
 };
