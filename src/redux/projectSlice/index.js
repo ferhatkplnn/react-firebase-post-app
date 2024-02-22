a589e4ccf6623d821816b1694da2439f5eae8ffd
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 11:46:46 +0300

    Add async thunk action to create a project in Firestore

--- src/redux/projectSlice/index.js
+++ src/redux/projectSlice/index.js
@@ -1,7 +1,21 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
+
+export const createProjectAsync = createAsyncThunk(
+  "project/createProjectAsync",
+  async (data) => {
+    const res = await addDoc(collection(db, "projects"), {
+      ...data,
+    });
+    console.log(res);
+  }
+);
 
 const initialState = {
   projects: [],
+  isLoading: false,
+  error: false,
 };
 
 const projectSlice = createSlice({
@@ -11,6 +25,18 @@ const projectSlice = createSlice({
     setProjects: (state, actions) => {
       state.projects = actions.payload;
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createProjectAsync.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createProjectAsync.fulfilled, (state) => {
+        state.isLoading = false;
+      })
+      .addCase(createProjectAsync.rejected, (state, action) => {
+        state.error = action.error.message;
+      });
   },
 });
 
