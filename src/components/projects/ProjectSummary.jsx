677b00e1906e9370c698cb8e39c1fab16a75ca91
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 22:35:55 +0300

    Truncate project content to one line.

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -3,7 +3,7 @@ const ProjectSummary = ({ title, content }) => {
     <>
       <div className="border flex flex-col p-12 bg-white drop-shadow-lg rounded-md space-y-1">
         <h2 className="text-4xl font-medium mb-4">{title}</h2>
-        <p className="text-xl">{content}</p>
+        <p className="text-xl line-clamp-1">{content}</p>
         <p className="text-lg text-slate-400">Today at 3:26 PM</p>
       </div>
     </>
