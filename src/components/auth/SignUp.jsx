40b527caaf2ae88b67b421a8477155943d51b56a
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 21:29:19 +0300

    Add email sign up functionality.

--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Input from "../shared/Input";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import Loading from "../shared/Loading";
 
 const SignUp = () => {
   const [inputs, setInputs] = useState({
@@ -8,21 +12,47 @@ const SignUp = () => {
     firstName: "",
     lastName: "",
   });
+  const [isError, setIsError] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    console.log(inputs);
+    setIsPending(true);
+    singUpEmailPassword();
   };
 
   const handleChange = (e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
 
+  const singUpEmailPassword = async () => {
+    try {
+      const { email: loginEmail, password: loginPassword } = inputs;
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      if (userCredential.user) navigate("/");
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        console.log("auth/email-already-in-use");
+        setIsError(true);
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setIsPending(false);
+    }
+  };
+
   return (
     <div className="container mx-auto bg-white p-8 max-w-xl mt-16 rounded-sm drop-shadow-lg">
       <form onSubmit={handleSubmit} className="flex flex-col space-y-12">
-        <h1 className="text-4xl font-sans font-medium">Sign Up</h1>
+        <h1 className="text-4xl font-sans font-medium flex space-x-3">
+          <span>Sign Up</span> {isPending && <Loading />}
+        </h1>
 
         <Input
           type="email"
@@ -56,7 +86,14 @@ const SignUp = () => {
           value={inputs.lastName}
         />
 
-        <button className="self-start px-4 py-2 bg-pink-500 text-white uppercase rounded-sm hover:brightness-125">
+        <p className="!mt-2 text-red-500 h-6">
+          {isError && `Email already in using.`}{" "}
+        </p>
+
+        <button
+          type="submit"
+          className="self-start px-4 py-2 bg-pink-500 text-white uppercase rounded-sm hover:brightness-125"
+        >
           Sign Up
         </button>
       </form>
