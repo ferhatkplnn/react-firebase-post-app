d0dac15f8e4991d66747573c3bd7290885298f1b
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 21:27:44 +0300

    Add Firebase auth state listener

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+import { setIsFetching, setUser } from "./redux/authSlice";
 
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        dispatch(setUser(user.uid));
+        dispatch(setIsFetching(false));
+        return;
+      }
+
+      dispatch(setUser(null));
+      dispatch(setIsFetching(false));
+    });
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Navbar />
