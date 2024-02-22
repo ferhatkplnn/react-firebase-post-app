234690d181265eeb3de63a4e45c4a3ef7c264450
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 22:15:08 +0300

    Widen the dashboard container to fill the screen.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,12 +3,14 @@ import Notifications from "./Notifications";
 
 const Dashboard = () => {
   return (
-    <div className=" container mx-auto flex justify-between gap-40 p-8">
-      <div className="flex flex-col space-y-8 basis-1/2">
-        <ProjectList />
-      </div>
-      <div className="basis-1/2">
-        <Notifications />
+    <div className="w-screen">
+      <div className="container mx-auto flex justify-between gap-40 p-8  ">
+        <div className="flex flex-col space-y-8 basis-1/2">
+          <ProjectList />
+        </div>
+        <div className="basis-1/2">
+          <Notifications />
+        </div>
       </div>
     </div>
   );
