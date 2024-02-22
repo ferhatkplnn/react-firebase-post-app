61fd55187583b27eecd3277a677abf61e2938567
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 21:28:52 +0300

    Add login with email and password functionality.

--- src/components/auth/SignIn.jsx
+++ src/components/auth/SignIn.jsx
@@ -1,40 +1,76 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Input from "../shared/Input";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
+import Loading from "../shared/Loading";
 
 const SignIn = () => {
   const [inputs, setInputs] = useState({ email: "", password: "" });
+  const [isError, setIsError] = useState(false);
+  const [isPending, setIsPending] = useState(false);
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e);
-    console.log(inputs);
+    setIsPending(true);
+    loginEmailPassword();
   };
 
   const handleChange = (e) => {
     setInputs({ ...inputs, [e.target.name]: e.target.value });
   };
 
+  const loginEmailPassword = async () => {
+    try {
+      const { email: loginEmail, password: loginPassword } = inputs;
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        loginEmail,
+        loginPassword
+      );
+      if (userCredential.user) navigate("/");
+    } catch (error) {
+      if (error.code === "auth/invalid-credential") {
+        setIsError(true);
+      } else {
+        console.log(error);
+      }
+    } finally {
+      setIsPending(false);
+    }
+  };
+
   return (
     <div className="container mx-auto bg-white p-8 max-w-xl mt-16 rounded-sm drop-shadow-lg">
       <form onSubmit={handleSubmit} className="flex flex-col space-y-12">
-        <h1 className="text-4xl font-sans font-medium">Sign In</h1>
+        <h1 className="text-4xl font-sans font-medium flex space-x-3">
+          <span>Sign Up</span> {isPending && <Loading />}
+        </h1>
 
         <Input
           type="email"
           placeholder="Email"
           name="email"
           handleChange={handleChange}
-          value={inputs.email}
+          value={inputs?.email}
         />
         <Input
           type="password"
           placeholder="Password"
           name="password"
           handleChange={handleChange}
-          value={inputs.password}
+          value={inputs?.password}
         />
 
-        <button className="self-start px-4 py-2 bg-pink-500 text-white uppercase rounded-sm hover:brightness-125">
+        <p className="!mt-2 text-red-500 h-6">
+          {isError && `Email or password is wrong.`}
+        </p>
+
+        <button
+          type="submit"
+          className="self-start px-4 py-2 bg-pink-500 text-white uppercase rounded-sm hover:brightness-125"
+        >
           Login
         </button>
       </form>
