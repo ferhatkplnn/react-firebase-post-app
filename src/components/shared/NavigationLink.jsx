b553860da2426388f297d896d10640c223fadce0
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 22:19:37 +0300

    Change the padding of the NavigationLink component.

--- src/components/shared/NavigationLink.jsx
+++ src/components/shared/NavigationLink.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const NavigationLink = ({ to, text, ...props }) => {
   return (
     <>
-      <NavLink className="py-8 px-2 hover:bg-slate-700" to={to} {...props}>
+      <NavLink className="py-8 px-5 hover:bg-slate-700" to={to} {...props}>
         {text}
       </NavLink>
     </>
