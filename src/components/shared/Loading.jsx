6fdfb5c26090ee8e5661d66d9aeaafb03e6419d5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 21:28:17 +0300

    Create Loading component

---
+++ src/components/shared/Loading.jsx
@@ -0,0 +1,11 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+
+const Loading = () => {
+  return (
+    <>
+      <AiOutlineLoading3Quarters className="animate-spin text-4xl" />
+    </>
+  );
+};
+
+export default Loading;
