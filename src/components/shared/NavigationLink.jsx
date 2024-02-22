aa7a3790fc9a753d394089a923f0d59e717c25df
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 21:30:12 +0300

    Update NavigationLink component to accept additional props

--- src/components/shared/NavigationLink.jsx
+++ src/components/shared/NavigationLink.jsx
@@ -1,9 +1,9 @@
 import { NavLink } from "react-router-dom";
 
-const NavigationLink = ({ to, text }) => {
+const NavigationLink = ({ to, text, ...props }) => {
   return (
     <>
-      <NavLink className="py-8 px-2 hover:bg-slate-700" to={to}>
+      <NavLink className="py-8 px-2 hover:bg-slate-700" to={to} {...props}>
         {text}
       </NavLink>
     </>
