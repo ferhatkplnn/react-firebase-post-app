e7060609b0f54373b5098603fc20e201907f8dab
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-23 00:43:24 +0300

    Add user initials to SignedInLinks component.

--- src/components/layout/SignedInLinks.jsx
+++ src/components/layout/SignedInLinks.jsx
@@ -2,8 +2,12 @@ import { NavLink } from "react-router-dom";
 import NavigationLink from "../shared/NavigationLink";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useSelector } from "react-redux";
 
 const SignedInLinks = () => {
+  const { userDetail } = useSelector((state) => state.auth);
+  console.log(userDetail);
+
   const handleLogOut = async () => {
     try {
       await signOut(auth);
@@ -22,7 +26,7 @@ const SignedInLinks = () => {
           to="/"
           className="bg-orange-800 h-16 w-16 rounded-full flex items-center justify-center mx-4 "
         >
-          NN
+          {userDetail?.initials?.toUpperCase()}
         </NavLink>
       </div>
     </>
