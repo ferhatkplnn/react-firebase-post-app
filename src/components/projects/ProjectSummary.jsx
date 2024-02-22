ea8c1dadaad5d8b2c8f7a1f907005509f173a9db
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-23 01:31:46 +0300

    Update project summary component to include project creator and creation date

--- src/components/projects/ProjectSummary.jsx
+++ src/components/projects/ProjectSummary.jsx
@@ -1,10 +1,20 @@
-const ProjectSummary = ({ title, content }) => {
+import moment from "moment";
+
+const ProjectSummary = ({ project }) => {
   return (
     <>
-      <div className="border flex flex-col p-12 bg-white drop-shadow-lg rounded-md space-y-1">
-        <h2 className="text-4xl font-medium mb-4">{title}</h2>
-        <p className="text-xl line-clamp-1">{content}</p>
-        <p className="text-lg text-slate-400">Today at 3:26 PM</p>
+      <div className="border flex flex-col p-8 pb-4 bg-white drop-shadow-lg rounded-md space-y-4">
+        <h2 className="text-4xl font-medium mb-4">{project.title}</h2>
+        <p className="text-xl line-clamp-1">{project.content}</p>
+
+        <div className="">
+          <p className="text-sm text-slate-400 italic">
+            Posted by {project.firstName} {project.lastName}
+          </p>
+          <p className="text-sm text-slate-400 italic">
+            {moment(project.createdAt).format("lll")}
+          </p>
+        </div>
       </div>
     </>
   );
