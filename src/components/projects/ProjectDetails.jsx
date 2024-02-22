3700d41c54383c31b5bfb6eaa9ebaf2d59ef4bb2
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-22 12:55:50 +0300

    Add `useSingleDataById` hook to fetch project data by ID. Display project details if data is available, otherwise display an error message.

--- src/components/projects/ProjectDetails.jsx
+++ src/components/projects/ProjectDetails.jsx
@@ -1,21 +1,32 @@
 import { useParams } from "react-router-dom";
+import { useSingleDataById } from "../../hooks";
 
 const ProjectDetails = () => {
   const { id } = useParams();
 
-  return (
-    <div className="container mx-auto mt-8  pt-8 bg-white w-3/5 flex flex-col space-y-8 drop-shadow-xl rounded-lg overflow-hidden">
-      <h1 className="text-4xl font-medium px-8">Project Title - {id}</h1>
-      <p className="px-8">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod culpa
-        sit, illo labore iste maiores minima minus suscipit dicta natus qui
-        accusantium deleniti animi, nihil esse tenetur? Culpa, atque similique.
-      </p>
-      <div className="bg-slate-100 px-8 py-4 italic text-sm text-slate-500/70">
-        <p>Posted by Mario Plummer</p>
-        <p>Toda at 12:36 PM</p>
+  const [projectData] = useSingleDataById(id);
+
+  if (projectData === null) {
+    return (
+      <div className="flex justify-center mt-20 text-3xl font-medium text-orange-400">
+        Something has gone wrong. Please try again later.
       </div>
-    </div>
+    );
+  }
+
+  return (
+    <>
+      {projectData && (
+        <div className="container mx-auto mt-8  pt-8 bg-white w-3/5 flex flex-col space-y-8 drop-shadow-xl rounded-lg overflow-hidden">
+          <h1 className="text-4xl font-medium px-8">{projectData?.title}</h1>
+          <p className="px-8">{projectData?.content}</p>
+          <div className="bg-slate-100 px-8 py-4 italic text-sm text-slate-500/70">
+            <p>Posted by Mario Plummer</p>
+            <p>Toda at 12:36 PM</p>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
