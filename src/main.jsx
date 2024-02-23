b4df0f33fb6ea9f5788346c9018a718374eb6710
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-23 18:24:57 +0300

    Rename Router file

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
-import { router } from "./Router/Router.jsx";
+import { router } from "./router/Router.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
 
