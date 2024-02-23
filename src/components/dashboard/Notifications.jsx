24b5122bc8e7fb3ae72300784cdaa52cbd7914e5
Ferhat Kaplan <fkmll2121@gmail.com>
2024-02-23 05:21:23 +0300

    Update notifications component to use Redux and real-time notifications

--- src/components/dashboard/Notifications.jsx
+++ src/components/dashboard/Notifications.jsx
@@ -1,32 +1,27 @@
+import moment from "moment";
+import { useRealTimeNotifications } from "../../hooks";
+import { useSelector } from "react-redux";
+
 const Notifications = () => {
+  const { notifications } = useSelector((state) => state.notification);
+
+  useRealTimeNotifications();
   return (
     <>
       <div className="bg-white p-8 flex flex-col rounded-lg drop-shadow-xl">
         <h2 className="text-4xl font-medium mb-10">Notification</h2>
         <div className="flex flex-col space-y-4">
-          <div>
-            <p className="text-xl">
-              <span className="text-red-500">Mario Plummer</span> Added a new
-              Project
-            </p>
-            <p className="text-slate-400 text-lg">3 hours ago</p>
-          </div>
-
-          <div>
-            <p className="text-xl">
-              <span className="text-red-500">Mario Plummer</span> Added a new
-              Project
-            </p>
-            <p className="text-slate-400 text-lg">3 hours ago</p>
-          </div>
-
-          <div>
-            <p className="text-xl">
-              <span className="text-red-500">Mario Plummer</span> Added a new
-              Project
-            </p>
-            <p className="text-slate-400 text-lg">3 hours ago</p>
-          </div>
+          {notifications.map((notification) => (
+            <div key={notification?.id}>
+              <p className="text-xl">
+                <span className="text-red-500">{notification?.user}</span>{" "}
+                {notification?.content}
+              </p>
+              <p className="text-slate-400 text-lg">
+                {moment(notification?.time).fromNow()}
+              </p>
+            </div>
+          ))}
         </div>
       </div>
     </>
